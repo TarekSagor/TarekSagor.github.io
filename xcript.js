bef927dff969bdeed346ed7aca4d5a1eca7b846e
TarekSagor <93405344+TarekSagor@users.noreply.github.com>
2021-10-30 01:47:12 +0600

    update toggle

--- xcript.js
+++ xcript.js
@@ -1,6 +1,16 @@
 var title = document.getElementById('head')
 
+var toggle = true
+
 function color() {
-    title.classList.remove('heading1')
-    title.classList.add('heading2')
+    if (toggle) {
+        toggle=!toggle
+        title.classList.remove('heading1')
+        title.classList.add('heading2')
+    } else {
+        toggle=!toggle
+        title.classList.remove('heading2')
+        title.classList.add('heading1')
+    }
+
 }
